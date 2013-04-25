19c0bc5c0278621e0f417d1480afb38a8783581e
Michael Anstis <michael.anstis@gmail.com>
2013-04-25 15:57:35 +0100

    Add Inbox to Authoring perspective

--- kie-ide-showcase/src/main/java/org/kie/client/perspectives/ProjectAuthoringPerspective.java
+++ kie-ide-showcase/src/main/java/org/kie/client/perspectives/ProjectAuthoringPerspective.java
@@ -19,10 +19,14 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.kie.client.resources.i18n.Constants;
+import org.kie.guvnor.commons.ui.client.handlers.NewResourcePresenter;
 import org.kie.guvnor.commons.ui.client.handlers.NewResourcesMenu;
+import org.kie.guvnor.inbox.client.InboxPresenter;
 import org.uberfire.client.annotations.Perspective;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPerspective;
+import org.uberfire.client.annotations.WorkbenchToolBar;
 import org.uberfire.client.mvp.Command;
 import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.client.workbench.Position;
@@ -33,45 +37,40 @@ import org.uberfire.client.workbench.model.impl.PartDefinitionImpl;
 import org.uberfire.client.workbench.model.impl.PerspectiveDefinitionImpl;
 import org.uberfire.client.workbench.widgets.menu.MenuFactory;
 import org.uberfire.client.workbench.widgets.menu.Menus;
+import org.uberfire.client.workbench.widgets.toolbar.IconType;
+import org.uberfire.client.workbench.widgets.toolbar.ToolBar;
+import org.uberfire.client.workbench.widgets.toolbar.impl.DefaultToolBar;
+import org.uberfire.client.workbench.widgets.toolbar.impl.DefaultToolBarItem;
+import org.uberfire.shared.mvp.PlaceRequest;
 import org.uberfire.shared.mvp.impl.DefaultPlaceRequest;
 
-/**
- * @author salaboy
- */
 @ApplicationScoped
 @WorkbenchPerspective(identifier = "Authoring")
 public class ProjectAuthoringPerspective {
 
     @Inject
-    private PlaceManager          placeManager;
-    private PerspectiveDefinition perspective;
-    private Menus                 menus;
+    private NewResourcePresenter newResourcePresenter;
 
     @Inject
     private NewResourcesMenu newResourcesMenu;
 
-    public ProjectAuthoringPerspective() {
-    }
+    @Inject
+    private PlaceManager placeManager;
+
+    private PerspectiveDefinition perspective;
+    private Menus menus;
+    private ToolBar toolBar;
 
     @PostConstruct
     public void init() {
-
+        buildPerspective();
         buildMenuBar();
-
+        buildToolBar();
     }
 
     @Perspective
     public PerspectiveDefinition getPerspective() {
-        final PerspectiveDefinition p = new PerspectiveDefinitionImpl();
-        p.setName( "Project Authoring Perspective" );
-
-        final PanelDefinition west = new PanelDefinitionImpl();
-        west.setWidth( 300 );
-        west.setMinWidth( 200 );
-        west.addPart( new PartDefinitionImpl( new DefaultPlaceRequest( "org.kie.guvnor.explorer" ) ) );
-        p.getRoot().insertChild( Position.WEST, west );
-        p.setTransient( true );
-        return p;
+        return this.perspective;
     }
 
     @WorkbenchMenu
@@ -79,19 +78,85 @@ public class ProjectAuthoringPerspective {
         return this.menus;
     }
 
+    @WorkbenchToolBar
+    public ToolBar getToolBar() {
+        return this.toolBar;
+    }
+
+    private void buildPerspective() {
+        this.perspective = new PerspectiveDefinitionImpl();
+        this.perspective.setName( "Project Authoring Perspective" );
+
+        final PanelDefinition west = new PanelDefinitionImpl();
+        west.setWidth( 300 );
+        west.setMinWidth( 200 );
+        west.addPart( new PartDefinitionImpl( new DefaultPlaceRequest( "org.kie.guvnor.explorer" ) ) );
+
+        this.perspective.getRoot().insertChild( Position.WEST, west );
+    }
+
     private void buildMenuBar() {
         this.menus = MenuFactory
-                .newTopLevelMenu( "Projects" )
-                    .respondsWith( new Command() {
-                        @Override
-                        public void execute() {
-                            placeManager.goTo( "org.kie.guvnor.explorer" );
-                        }
-                    } )
+                .newTopLevelMenu( "Explore" )
+                .menus()
+                .menu( "Projects" )
+                .respondsWith( new Command() {
+                    @Override
+                    public void execute() {
+                        placeManager.goTo( "org.kie.guvnor.explorer" );
+                    }
+                } )
+                .endMenu()
+                .menu( "Incoming Changes" )
+                .respondsWith( new Command() {
+                    @Override
+                    public void execute() {
+                        //PlaceRequest p = new PathPlaceRequest("Inbox");
+                        //p.addParameter("inboxname", InboxPresenter.INCOMING_ID);
+                        placeManager.goTo( "Inbox" );
+                    }
+                } )
+                .endMenu()
+                .menu( "Recently Edited" )
+                .respondsWith( new Command() {
+                    @Override
+                    public void execute() {
+                        PlaceRequest p = new DefaultPlaceRequest( "Inbox" );
+                        p.addParameter( "inboxname", InboxPresenter.RECENT_EDITED_ID );
+                        placeManager.goTo( p );
+                    }
+                } )
+                .endMenu()
+                .menu( "Recently Opened" )
+                .respondsWith( new Command() {
+                    @Override
+                    public void execute() {
+                        PlaceRequest p = new DefaultPlaceRequest( "Inbox" );
+                        p.addParameter( "inboxname", InboxPresenter.RECENT_VIEWED_ID );
+                        placeManager.goTo( p );
+                    }
+                } )
+                .endMenu()
+                .endMenus()
                 .endMenu()
                 .newTopLevelMenu( "New" )
-                    .withItems( newResourcesMenu.getMenuItems() )
+                .withItems( newResourcesMenu.getMenuItems() )
                 .endMenu().build();
+    }
+
+    private void buildToolBar() {
+        this.toolBar = new DefaultToolBar( "guvnor.new.item" );
+        final String tooltip = Constants.INSTANCE.newItem();
+        final Command command = new Command() {
+            @Override
+            public void execute() {
+                newResourcePresenter.show();
+            }
+        };
+        toolBar.addItem( new DefaultToolBarItem( IconType.FILE,
+                                                 tooltip,
+                                                 command ) );
 
     }
+
 }
