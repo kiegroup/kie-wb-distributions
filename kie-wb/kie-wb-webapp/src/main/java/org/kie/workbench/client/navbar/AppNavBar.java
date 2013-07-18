8d51ed74370c6ae9dd76d152b4f5c21c7c18503b
Michael Anstis <michael.anstis@gmail.com>
2013-07-18 10:05:42 +0100

    Reorder Header widgets to be consistent with Uberfire (and a more natural hierachy of menus)

--- kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/client/navbar/AppNavBar.java
+++ kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/client/navbar/AppNavBar.java
@@ -8,6 +8,8 @@ import com.google.gwt.user.client.ui.Widget;
 import org.uberfire.client.workbench.Header;
 import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBarPresenter;
 
+import static java.lang.Integer.MAX_VALUE;
+
 @ApplicationScoped
 public class AppNavBar
         extends Composite implements Header {
@@ -22,6 +24,6 @@ public class AppNavBar
 
     @Override
     public int getOrder() {
-        return 0;
+        return MAX_VALUE;
     }
 }
