ed8b5f2777521bc7f838444da8fcb507b323498f
Michael Anstis <michael.anstis@gmail.com>
2013-11-13 14:36:14 +0000

    BZ1029839 - Add splash screens for perspectives (cherry picked from commit 17ad423d0fd20bd14cfcce1974894a2a37758111)

---
+++ kie-drools-wb/kie-drools-wb-webapp/src/main/webapp/plugins/authoring_perspective.splash.js
@@ -0,0 +1,9 @@
+$registerSplashScreen({
+    id: "authoring_perspective.splash",
+    templateUrl: "authoring_perspective.splash.html",
+    title: function () {
+        return "Authoring quick start";
+    },
+    display_next_time: true,
+    interception_points: ["org.kie.workbench.drools.client.perspectives.DroolsAuthoringPerspective"]
+});
