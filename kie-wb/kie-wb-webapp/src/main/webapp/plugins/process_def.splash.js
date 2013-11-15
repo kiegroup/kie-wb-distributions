35b41db0f9f4364a6e579dd2542016bb539d7a87
Salaboy <salaboy@gmail.com>
2013-11-15 12:41:15 +0000

    - updating splash screens for the bpm side

--- kie-wb/kie-wb-webapp/src/main/webapp/plugins/process_def.splash.js
+++ kie-wb/kie-wb-webapp/src/main/webapp/plugins/process_def.splash.js
@@ -1,9 +1,9 @@
 $registerSplashScreen({
-    id: 'process-def.splash',
-    templateUrl: 'process-def.splash.html',
+    id: 'process_def.splash',
+    templateUrl: 'process_def.splash.html',
     title: function () {
         return 'Process Definitions';
     },
     display_next_time: true,
-    interception_points: ['org.jbpm.console.ng.pr.client.perspectives.ProcessDefinitionsPerspective']
+    interception_points: ['Process Definitions']
 });
