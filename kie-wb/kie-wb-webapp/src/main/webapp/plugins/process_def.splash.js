fe0ed8095ea0abe4f6a52ba170a86f4323a6020a
Michael Anstis <michael.anstis@gmail.com>
2013-11-17 13:45:03 +0000

    Revert "Remove Splash Screens from product branch"
    This reverts commit ed1d2d10271db8f0d13e98acba3d937b0ecb1c9e.

---
+++ kie-wb/kie-wb-webapp/src/main/webapp/plugins/process_def.splash.js
@@ -0,0 +1,9 @@
+$registerSplashScreen({
+    id: 'process_def.splash',
+    templateUrl: 'process_def.splash.html',
+    title: function () {
+        return 'Process Definitions';
+    },
+    display_next_time: true,
+    interception_points: ['Process Definitions']
+});
---
+++ kie-wb/kie-wb-webapp/src/main/webapp/plugins/process_inst.splash.js
@@ -0,0 +1,9 @@
+$registerSplashScreen({
+    id: 'process_inst.splash',
+    templateUrl: 'process_inst.splash.html',
+    title: function () {
+        return 'Process Instances';
+    },
+    display_next_time: true,
+    interception_points: ['Process Instances']
+});
