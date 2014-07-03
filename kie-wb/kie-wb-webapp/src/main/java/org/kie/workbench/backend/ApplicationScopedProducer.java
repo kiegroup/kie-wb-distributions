4bd38c631e7c589970afad47382bc1a75d5d8c97
Maciej Swiderski <mswiders@redhat.com>
2014-07-03 10:29:27 +0200

    BZ-1098226 - Inaccessible JBoss EWS container after a few REST requests - change to be included in dependent scope beans but ensures only single instance of EntityManagerFactory is created

--- kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/backend/ApplicationScopedProducer.java
+++ kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/backend/ApplicationScopedProducer.java
@@ -1,27 +1,13 @@
 package org.kie.workbench.backend;
 
-import java.util.Properties;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceUnit;
 
 import org.guvnor.common.services.backend.metadata.attribute.OtherMetaView;
-import org.jbpm.runtime.manager.impl.DefaultRuntimeEnvironment;
-import org.jbpm.runtime.manager.impl.SimpleRuntimeEnvironment;
-import org.jbpm.services.task.identity.JBossUserGroupCallbackImpl;
-import org.kie.internal.runtime.manager.RuntimeEnvironment;
-import org.kie.internal.runtime.manager.cdi.qualifier.PerProcessInstance;
-import org.kie.internal.runtime.manager.cdi.qualifier.PerRequest;
-import org.kie.internal.runtime.manager.cdi.qualifier.Singleton;
 import org.uberfire.backend.server.IOWatchServiceNonDotImpl;
 import org.uberfire.backend.server.io.IOSecurityAuth;
 import org.uberfire.backend.server.io.IOSecurityAuthz;
@@ -32,7 +18,6 @@ import org.uberfire.io.attribute.DublinCoreView;
 import org.uberfire.io.impl.cluster.IOServiceClusterImpl;
 import org.uberfire.java.nio.base.version.VersionAttributeView;
 import org.uberfire.metadata.backend.lucene.LuceneConfig;
-import org.uberfire.metadata.backend.lucene.LuceneConfigBuilder;
 import org.uberfire.metadata.io.IOSearchIndex;
 import org.uberfire.metadata.io.IOServiceIndexedImpl;
 import org.uberfire.security.auth.AuthenticationManager;
@@ -46,27 +31,6 @@ import org.uberfire.security.server.cdi.SecurityFactory;
  */
 @ApplicationScoped
 public class ApplicationScopedProducer {
-
-    /**
-     * If the EMF is not produced by an @ApplicationScoped bean, 
-     * then tomcat will create an emf *per request*, leading to a memory leak
-     */
-    @PersistenceUnit(unitName = "org.jbpm.domain")
-    private EntityManagerFactory emf;
-
-    @Produces
-    public EntityManagerFactory getEntityManagerFactory() {
-        if ( this.emf == null ) {
-            // this needs to be here for non EE containers
-            try {
-                this.emf = InitialContext.doLookup("jBPMEMF");
-            } catch ( NamingException e ) {
-                this.emf = Persistence.createEntityManagerFactory("org.jbpm.domain");
-            }
-
-        }
-        return this.emf;
-    }
     
     @Inject
     @IOSecurityAuth
@@ -139,20 +103,5 @@ public class ApplicationScopedProducer {
     public IOSearchService ioSearchService() {
         return ioSearchService;
     }
-
-
-
-    @Produces
-    @Singleton
-    @PerRequest
-    @PerProcessInstance
-    public RuntimeEnvironment produceEnvironment( EntityManagerFactory emf ) {
-        SimpleRuntimeEnvironment environment = new DefaultRuntimeEnvironment( emf );
-        Properties properties = new Properties();
-        environment.setUserGroupCallback( new JBossUserGroupCallbackImpl( properties ) );
-        return environment;
-    }
-
-
 
 }
--- kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/backend/BpmDependenciesProducer.java
+++ kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/backend/BpmDependenciesProducer.java
@@ -3,8 +3,14 @@ package org.kie.workbench.backend;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
 
 import org.jbpm.kie.services.cdi.producer.UserGroupInfoProducer;
+import org.jbpm.runtime.manager.impl.jpa.EntityManagerFactoryManager;
 import org.jbpm.services.task.audit.JPATaskLifeCycleEventListener;
 import org.jbpm.services.task.lifecycle.listeners.BAMTaskEventListener;
 import org.jbpm.shared.services.cdi.Selectable;
@@ -12,6 +18,23 @@ import org.kie.api.task.TaskLifeCycleEventListener;
 import org.kie.internal.task.api.UserInfo;
 
 public class BpmDependenciesProducer {
+
+    @PersistenceUnit(unitName = "org.jbpm.domain")
+    private EntityManagerFactory emf;
+
+    @Produces
+    public EntityManagerFactory getEntityManagerFactory() {
+        if ( this.emf == null ) {
+            // this needs to be here for non EE containers
+            try {
+                this.emf = InitialContext.doLookup("jBPMEMF");
+            } catch ( NamingException e ) {
+                this.emf = EntityManagerFactoryManager.get().getOrCreate("org.jbpm.domain");
+            }
+
+        }
+        return this.emf;
+    }
 
     @Inject
     @Selectable
