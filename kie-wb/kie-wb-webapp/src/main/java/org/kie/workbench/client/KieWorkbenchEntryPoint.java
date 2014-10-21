81c2fe9fbcc162b05f55d220d93eebde56cdb7c8
Maciej Swiderski <mswiders@redhat.com>
2014-10-21 15:08:00 +0200

    use logout page to properly handle logout on various containers
    (cherry picked from commit f6ab94d44e4908e3bc07e35a4f123e47e361ad2c)

--- kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/client/KieWorkbenchEntryPoint.java
+++ kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/client/KieWorkbenchEntryPoint.java
@@ -409,7 +409,7 @@ public class KieWorkbenchEntryPoint {
             authService.call( new RemoteCallback<Void>() {
                 @Override
                 public void callback( Void response ) {
-                    final String location = GWT.getModuleBaseURL().replaceFirst("/" + GWT.getModuleName() + "/", "");
+                    final String location = GWT.getModuleBaseURL().replaceFirst("/" + GWT.getModuleName() + "/",  "/logout.jsp");
                     redirect( location );
                 }
             } ).logout();
