fb32378ab2004c0e4feb99256334bd5a7120e894
Walter Medvedeo <wmedvedeo@gmail.com>
2014-11-05 09:36:20 +0100

    Create Repository Wizzard integration

--- kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/client/perspectives/AdministrationPerspective.java
+++ kie-wb/kie-wb-webapp/src/main/java/org/kie/workbench/client/perspectives/AdministrationPerspective.java
@@ -22,8 +22,8 @@ import javax.inject.Inject;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.ui.PopupPanel;
+import org.guvnor.asset.management.client.editors.repository.wizard.CreateRepositoryWizard;
 import org.guvnor.structure.client.editors.repository.clone.CloneRepositoryForm;
-import org.guvnor.structure.client.editors.repository.create.CreateRepositoryForm;
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.kie.workbench.client.resources.i18n.AppConstants;
 import org.kie.workbench.common.services.security.KieWorkbenchACL;
@@ -31,6 +31,7 @@ import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
 import org.uberfire.client.annotations.Perspective;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPerspective;
+import org.uberfire.client.callbacks.Callback;
 import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.client.workbench.panels.impl.MultiListWorkbenchPanelPresenter;
 import org.uberfire.client.workbench.panels.impl.SimpleWorkbenchPanelPresenter;
@@ -153,15 +154,14 @@ public class AdministrationPerspective {
         this.newRepoCommand = new Command() {
             @Override
             public void execute() {
-                final CreateRepositoryForm newRepositoryWizard = iocManager.lookupBean( CreateRepositoryForm.class ).getInstance();
+                final CreateRepositoryWizard newRepositoryWizard = iocManager.lookupBean( CreateRepositoryWizard.class ).getInstance();
                 //When pop-up is closed destroy bean to avoid memory leak
-                newRepositoryWizard.addCloseHandler( new CloseHandler<CreateRepositoryForm>() {
-                    @Override
-                    public void onClose( CloseEvent<CreateRepositoryForm> event ) {
+                newRepositoryWizard.onCloseCallback( new Callback<Void>() {
+                    @Override public void callback( Void result ) {
                         iocManager.destroyBean( newRepositoryWizard );
                     }
                 } );
-                newRepositoryWizard.show();
+                newRepositoryWizard.start();
             }
         };
     }
