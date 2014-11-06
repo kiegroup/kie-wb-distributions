a0c602b2cad4509a48c6eb832bfeefe4f150d92e
Toni Rikkola <toni@rikkola.net>
2014-11-06 11:42:13 +0200

    Change branch in GitRepository changed
    (cherry picked from commit 5cf5525fcdda086f3c15f7985051cf0a5bb77108)

--- kie-config-cli/src/main/java/org/kie/config/cli/support/SimplifiedGitRepoHelper.java
+++ kie-config-cli/src/main/java/org/kie/config/cli/support/SimplifiedGitRepoHelper.java
@@ -1,8 +1,5 @@
 package org.kie.config.cli.support;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
 import org.guvnor.structure.backend.repositories.EnvironmentParameters;
 import org.guvnor.structure.repositories.Repository;
 import org.guvnor.structure.repositories.impl.git.GitRepository;
@@ -11,6 +8,9 @@ import org.guvnor.structure.server.config.ConfigItem;
 import org.guvnor.structure.server.config.PasswordService;
 import org.guvnor.structure.server.config.SecureConfigItem;
 import org.guvnor.structure.server.repositories.RepositoryFactoryHelper;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import static org.guvnor.structure.repositories.impl.git.GitRepository.SCHEME;
 import static org.uberfire.commons.validation.PortablePreconditions.checkNotNull;
@@ -22,39 +22,40 @@ public class SimplifiedGitRepoHelper implements RepositoryFactoryHelper {
     private PasswordService secureService;
 
     @Override
-    public boolean accept( final ConfigGroup repoConfig ) {
-        checkNotNull( "repoConfig", repoConfig );
-        final ConfigItem<String> schemeConfigItem = repoConfig.getConfigItem( EnvironmentParameters.SCHEME );
-        checkNotNull( "schemeConfigItem", schemeConfigItem );
-        return SCHEME.equals( schemeConfigItem.getValue() );
+    public boolean accept(final ConfigGroup repoConfig) {
+        checkNotNull("repoConfig", repoConfig);
+        final ConfigItem<String> schemeConfigItem = repoConfig.getConfigItem(EnvironmentParameters.SCHEME);
+        checkNotNull("schemeConfigItem", schemeConfigItem);
+        return SCHEME.equals(schemeConfigItem.getValue());
     }
 
     @Override
-    public Repository newRepository( final ConfigGroup repoConfig ) {
-        checkNotNull( "repoConfig", repoConfig );
-        final ConfigItem<String> schemeConfigItem = repoConfig.getConfigItem( EnvironmentParameters.SCHEME );
-        checkNotNull( "schemeConfigItem", schemeConfigItem );
+    public Repository newRepository(final ConfigGroup repoConfig) {
+        checkNotNull("repoConfig", repoConfig);
+        final ConfigItem<String> schemeConfigItem = repoConfig.getConfigItem(EnvironmentParameters.SCHEME);
+        checkNotNull("schemeConfigItem", schemeConfigItem);
 
-        String branch = repoConfig.getConfigItemValue( EnvironmentParameters.BRANCH );
-        if ( branch == null ) {
+        String branch = repoConfig.getConfigItemValue(EnvironmentParameters.BRANCH);
+        if (branch == null) {
             branch = "master";
         }
 
-        final GitRepository repo = new GitRepository( repoConfig.getName() );
-        repo.setCurrentBranch( branch );
+        final GitRepository repo = new GitRepository(repoConfig.getName());
+//        repo.changeBranch( branch );
 
-        for ( final ConfigItem item : repoConfig.getItems() ) {
-            if ( item instanceof SecureConfigItem) {
-                repo.addEnvironmentParameter( item.getName(), secureService.decrypt( item.getValue().toString() ) );
+        for (final ConfigItem item : repoConfig.getItems()) {
+            if (item instanceof SecureConfigItem) {
+                repo.addEnvironmentParameter(item.getName(), secureService.decrypt(item.getValue().toString()));
             } else {
-                repo.addEnvironmentParameter( item.getName(), item.getValue() );
+                repo.addEnvironmentParameter(item.getName(), item.getValue());
             }
         }
 
-        if ( !repo.isValid() ) {
-            throw new IllegalStateException( "Repository " + repoConfig.getName() + " not valid" );
+        if (!repo.isValid()) {
+            throw new IllegalStateException("Repository " + repoConfig.getName() + " not valid");
         }
 
         return repo;
     }
+
 }
