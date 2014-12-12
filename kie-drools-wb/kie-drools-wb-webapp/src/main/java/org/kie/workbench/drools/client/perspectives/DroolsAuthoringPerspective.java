7a71278e3325cbb12448d17065ba200d9a4c0113
Walter Medvedeo <wmedvedeo@gmail.com>
2014-12-12 11:49:12 +0100

    BZ-1172150 Duplicate link to Repository structure
    (cherry picked from commit cd4ae167d1f52587e35dbca92f2b9152743dfb87)

--- kie-drools-wb/kie-drools-wb-webapp/src/main/java/org/kie/workbench/drools/client/perspectives/DroolsAuthoringPerspective.java
+++ kie-drools-wb/kie-drools-wb-webapp/src/main/java/org/kie/workbench/drools/client/perspectives/DroolsAuthoringPerspective.java
@@ -19,7 +19,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.guvnor.inbox.client.InboxPresenter;
-import org.kie.workbench.common.screens.projecteditor.client.menu.ProjectMenu;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcesMenu;
 import org.kie.workbench.common.widgets.client.menu.RepositoryMenu;
@@ -56,9 +55,6 @@ public class DroolsAuthoringPerspective {
 
     @Inject
     private NewResourcesMenu newResourcesMenu;
-
-    @Inject
-    private ProjectMenu projectMenu;
 
     @Inject
     private PlaceManager placeManager;
@@ -117,9 +113,6 @@ public class DroolsAuthoringPerspective {
                 .endMenu()
                 .newTopLevelMenu( constants.newItem() )
                 .withItems( newResourcesMenu.getMenuItems() )
-                .endMenu()
-                .newTopLevelMenu( constants.tools() )
-                .withItems( projectMenu.getMenuItems() )
                 .endMenu()
                 .newTopLevelMenu( constants.Repository() )
                 .withItems( repositoryMenu.getMenuItems() )
