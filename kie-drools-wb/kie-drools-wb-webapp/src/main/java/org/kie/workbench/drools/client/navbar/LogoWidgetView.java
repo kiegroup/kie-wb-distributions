b3d614d01bb43f085e249cabb9cd201a0baebb64
Michael Anstis <michael.anstis@gmail.com>
2014-12-15 20:42:07 +0000

    BZ1169634: Ability to easily change the branding of the Business Central

--- kie-drools-wb/kie-drools-wb-webapp/src/main/java/org/kie/workbench/drools/client/navbar/LogoWidgetView.java
+++ kie-drools-wb/kie-drools-wb-webapp/src/main/java/org/kie/workbench/drools/client/navbar/LogoWidgetView.java
@@ -14,18 +14,15 @@
  * limitations under the License.
  */
 
-package org.kie.workbench.client.navbar;
+package org.kie.workbench.drools.client.navbar;
 
 import javax.annotation.PostConstruct;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RequiresResize;
-import org.kie.workbench.client.resources.AppResource;
 
 /**
  * A stand-alone (i.e. devoid of Workbench dependencies) View
@@ -43,13 +40,8 @@ public class LogoWidgetView
 
     private static ViewBinder uiBinder = GWT.create( ViewBinder.class );
 
-    @UiField(provided = true)
-    public Image logo;
-
-
     @PostConstruct
     public void init() {
-        logo = new Image( AppResource.INSTANCE.images().logo() );
         initWidget( uiBinder.createAndBindUi( this ) );
     }
 
