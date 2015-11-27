92dc05086af1d7d8f13246deeeab12ec2a4af0e1
Jan Hrcek <jhrcek@redhat.com>
2015-11-27 14:12:30 +0100

    Fix selenium LoginIntegrationTest

--- kie-wb-smoke-tests/src/main/java/org/kie/smoke/wb/selenium/model/PrimaryNavbar.java
+++ kie-wb-smoke-tests/src/main/java/org/kie/smoke/wb/selenium/model/PrimaryNavbar.java
@@ -22,31 +22,35 @@ import org.openqa.selenium.WebElement;
 
 public class PrimaryNavbar extends PageObject {
 
-    private static final String TOP_MENU //Contains both the link to expand menu as well as menu item links
-            = "//ul[contains(@class,'navbar-primary')] / li[contains(@class,'dropdown')] [a[contains(text(),'%s')]]";
+    private static final String NAV_MENU //Contains both the link to expand menu as well as menu item links
+            = "//nav//li[@class='dropdown'][a[contains(text(),'%s')]]";
+    private static final By LOGOUT_MENU // Unlike other navbar menus this has no stable link text
+            = By.cssSelector("li[title='Reset all Perspective layouts']+li");
 
     public PrimaryNavbar(WebDriver driver) {
         super(driver);
     }
 
     private WebElement openMenu(String menuTitle) {
-        WebElement menu = driver.findElement(xpath(TOP_MENU, menuTitle));
+        WebElement menu = driver.findElement(xpath(NAV_MENU, menuTitle));
         menu.findElement(By.tagName("a")).click();
         return menu;
     }
 
-    private void navigateTo(String menuText, String itemText) {
-        WebElement menu = openMenu(menuText);
-        WebElement menuItem = menu.findElement(By.linkText(itemText));
+    private void selectItem(WebElement openedMenu, String itemText) {
+        WebElement menuItem = openedMenu.findElement(By.linkText(itemText));
         menuItem.click();
     }
 
     public void logout() {
-        navigateTo("User:", "Log Out");
+        WebElement menu = driver.findElement(LOGOUT_MENU);
+        menu.findElement(By.tagName("a")).click();
+        selectItem(menu, "Log Out");
     }
 
     public void projectAuthoring() {
-        navigateTo("Authoring", "Project Authoring");
+        WebElement menu = openMenu("Authoring");
+        selectItem(menu, "Project Authoring");
     }
 
     //TODO all items to navigate to other perspectivesS
