31743bce345489ffd8a08a0d4f5fd46bd499e075
Paulo Martins <paulovmr@gmail.com>
2017-07-28 08:28:44 -0300

    GUVNOR-3237: Change the home page to the new one (#560)

--- kie-drools-wb-parent/kie-drools-wb-home-page-community/src/main/java/org/kie/workbench/drools/client/home/HomeProducer.java
+++ kie-drools-wb-parent/kie-drools-wb-home-page-community/src/main/java/org/kie/workbench/drools/client/home/HomeProducer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,67 +16,46 @@
 package org.kie.workbench.drools.client.home;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.Dependent;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
-import com.google.gwt.core.client.GWT;
+import org.jboss.errai.ui.client.local.spi.TranslationService;
 import org.kie.workbench.common.screens.home.model.HomeModel;
+import org.kie.workbench.common.screens.home.model.HomeModelProvider;
 import org.kie.workbench.common.screens.home.model.ModelUtils;
-import org.kie.workbench.common.screens.home.model.SectionEntry;
-import org.kie.workbench.drools.client.resources.i18n.HomePageCommunityConstants;
+import org.kie.workbench.drools.client.resources.i18n.Constants;
 import org.uberfire.client.mvp.PlaceManager;
 
-import static org.kie.workbench.common.workbench.client.PerspectiveIds.ADMINISTRATION;
-import static org.kie.workbench.common.workbench.client.PerspectiveIds.GUVNOR_M2REPO;
 import static org.kie.workbench.common.workbench.client.PerspectiveIds.LIBRARY;
 import static org.kie.workbench.common.workbench.client.PerspectiveIds.SERVER_MANAGEMENT;
 import static org.uberfire.workbench.model.ActivityResourceType.PERSPECTIVE;
 
-/**
- * Producer method for the Home Page content
- */
-@Dependent
-public class HomeProducer {
+@ApplicationScoped
+public class HomeProducer implements HomeModelProvider {
 
-    @Produces
-    @ApplicationScoped
-    public HomeModel getModel(PlaceManager placeManager) {
-        final HomePageCommunityConstants constants = HomePageCommunityConstants.INSTANCE;
-        final String url = GWT.getModuleBaseURL();
-        final HomeModel model = new HomeModel( constants.homeTheKnowledgeLifeCycle() );
-        model.addCarouselEntry( ModelUtils.makeCarouselEntry( constants.homeAuthor(),
-                                                              constants.homeAuthorCaption(),
-                                                              url + "/images/HandHome.jpg" ) );
-        model.addCarouselEntry( ModelUtils.makeCarouselEntry( constants.homeDeploy(),
-                                                              constants.homeDeployCaption(),
-                                                              url + "/images/HandHome.jpg" ) );
+    @Inject
+    private PlaceManager placeManager;
 
-        final SectionEntry s1 = ModelUtils.makeSectionEntry( constants.authoring() );
+    @Inject
+    private TranslationService translationService;
 
-        s1.addChild( ModelUtils.makeSectionEntry( constants.project_authoring(),
-                () -> placeManager.goTo( LIBRARY ),
-                LIBRARY, PERSPECTIVE ) );
+    public HomeModel get() {
+        final HomeModel model = new HomeModel(translationService.format(Constants.Heading),
+                                              translationService.format(Constants.SubHeading),
+                                              "images/community_home_bg.jpg");
 
-        s1.addChild( ModelUtils.makeSectionEntry( constants.artifactRepository(),
-                () -> placeManager.goTo( GUVNOR_M2REPO ),
-                GUVNOR_M2REPO, PERSPECTIVE ) );
-
-        s1.addChild( ModelUtils.makeSectionEntry( constants.administration(),
-                () -> placeManager.goTo( ADMINISTRATION ),
-                ADMINISTRATION, PERSPECTIVE ) );
-
-        final SectionEntry s2 = ModelUtils.makeSectionEntry( constants.deploy() );
-
-        s2.addChild( ModelUtils.makeSectionEntry( constants.executionServers(),
-                () -> placeManager.goTo( SERVER_MANAGEMENT ),
-                SERVER_MANAGEMENT, PERSPECTIVE ) );
-
-
-        model.addSection( s1 );
-        model.addSection( s2 );
+        model.addShortcut(ModelUtils.makeShortcut("pficon-blueprint",
+                                                  translationService.format(Constants.Design),
+                                                  translationService.format(Constants.DesignDescription),
+                                                  () -> placeManager.goTo(LIBRARY),
+                                                  LIBRARY,
+                                                  PERSPECTIVE));
+        model.addShortcut(ModelUtils.makeShortcut("pficon-build",
+                                                  translationService.format(Constants.DevOps),
+                                                  translationService.format(Constants.DevOpsDescription),
+                                                  () -> placeManager.goTo(SERVER_MANAGEMENT),
+                                                  SERVER_MANAGEMENT,
+                                                  PERSPECTIVE));
 
         return model;
     }
-
 }
