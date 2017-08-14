8aba707b8ae35790180c025588aa7a77d9cf2c0c
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2017-08-14 17:58:43 +1000

    JBPM-6148 - Combine common web resources into new module (#582)

--- kie-drools-wb-parent/kie-drools-wb-webapp/src/main/webapp/plugins/apps.splash.js
+++
@@ -1,8 +0,0 @@
-$registerSplashScreen({
-    id: "apps.splash",
-    templateUrl: "apps.splash.html",
-    body_height: 380,
-    title: "Help",
-    display_next_time: false,
-    interception_points: ["AppsPerspective"]
-});
--- kie-wb-parent/kie-wb-webapp/src/main/webapp/plugins/authoring_perspective.splash.js
+++
@@ -1,8 +0,0 @@
-$registerSplashScreen({
-    id: "authoring_perspective.splash",
-    templateUrl: "authoring_perspective.splash.html",
-    body_height: 391,
-    title: "Help",
-    display_next_time: false,
-    interception_points: ["AuthoringPerspective"]
-});
