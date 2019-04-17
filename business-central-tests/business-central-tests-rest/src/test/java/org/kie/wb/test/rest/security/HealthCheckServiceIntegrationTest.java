c847e26cba749d01dc7ed963935a824642abda2f
Tiago Bento <1584568+tiagobento@users.noreply.github.com>
2019-04-17 12:24:30 -0300

    AF-1821 - Improve health check service (#912)
    * Exclude paths /rest/ready and /rest/healthy from BasicAuthSecurityFilter
    
    * Register public healthchecking endpoints as public resources
    
    * Fix web.xml
    
    * Update workbench client to handle ready and health endpoints
    
    * AF-1787: Readiness and Liveness endpoints IT test
    
    Add class for health and ready endpoints tests.
    Update client to be able to handle these endpoints.
    
    * Ensure HealthCheckService is accessible without authentication

---
+++ business-central-tests/business-central-tests-rest/src/test/java/org/kie/wb/test/rest/security/HealthCheckServiceIntegrationTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.wb.test.rest.security;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.wb.test.rest.RestTestBase;
+import org.kie.wb.test.rest.client.WorkbenchClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.kie.wb.test.rest.client.RestWorkbenchClient.createAsyncWorkbenchClient;
+
+public class HealthCheckServiceIntegrationTest extends RestTestBase {
+
+    private static final String SUCCESS = "success";
+
+    private static WorkbenchClient asyncClient;
+
+    @BeforeClass
+    public static void setUp() {
+        asyncClient = createAsyncWorkbenchClient(URL, null, null);
+    }
+
+    @Test
+    public void testGetReady() {
+        assertThat(asyncClient.isReady()).contains(SUCCESS).contains(String.valueOf(true));
+    }
+
+    @Test
+    public void testGetHealthy() {
+        assertThat(asyncClient.isHealthy()).contains("success").contains(String.valueOf(true));
+    }
+}
